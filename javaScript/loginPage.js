78ecb2a55ee22ca76751b4dab3ddf21c34ab21f6
nelsaj <112635402+nelsaj@users.noreply.github.com>
2023-04-25 12:10:40 +0200

    made loginpage

--- javaScript/loginPage.js
+++ javaScript/loginPage.js
@@ -1,3 +1,52 @@
 function renderLoginPage() {
+    main.innerHTML =
+        `
+            <h2>Login</h2>
+            <p id=message></p>
+            <form>
+                <input type=text id=username placeholder=Username or eMail>
+                <input type=password id=password placeholder=Password>
+                <button type=submit>Login</button>
+            </form>
+            <button id=register>New to this? Sign up for free</button>
+        `;
 
+    // go to register
+    main.querySelector("#register").addEventListener("click", renderRegisterPage);
+
+    let loginForm = main.querySelector("form");
+    let username = main.querySelector("#username");
+    let password = main.querySelector("#password");
+
+    loginForm.addEventListener("submit", async function (event) {
+        event.preventDefault();
+        let errorMessage = main.querySelector("#message");
+
+        try { // trying to log in...
+            let response = await fetch("../loginregister-api/login.php", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                }),
+            });
+
+            let data = await response.json();
+
+            if (!response.ok) {
+                errorMessage.innerHTML = `<span>${data.message}</span>.`; // error message
+            } else {
+                // add to local storage
+                window.localStorage.setItem("user", JSON.stringify(data));
+
+                user = data;
+
+                // logged in! (adding function later)
+                // renderSomethingPage();
+            }
+        } catch (err) { // if something went wrong
+            errorMessage.textContent = `Error: ${err.message}`;
+        }
+    });
 }
