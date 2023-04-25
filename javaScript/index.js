74d8206f6dfbc6547102da153fc439349c37b4f3
Linnéa <linnea.a.14@gmail.com>
2023-04-25 10:33:53 +0100

    StartPage

---
+++ javaScript/index.js
@@ -0,0 +1,8 @@
+// The user starts out as `null` until we've logged in
+let user = null;
+// This is where we render all of our pages
+let main = document.querySelector("main");
+
+if (!window.localStorage.getItem("quiz-user")) {
+    renderStartPage();
+}
