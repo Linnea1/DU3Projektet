205e8416b0b665badffe1b49eaa719addfe15420
Linnéa <linnea.a.14@gmail.com>
2023-04-25 10:49:42 +0100

    categories

--- javaScript/startPage.js
+++ javaScript/startPage.js
@@ -1,7 +1,7 @@
 function renderStartPage(){
     main.innerHTML=`
     <h2>Welcome to YumYumClub</h2>
-    <div class=image>Welcome to YumYumClub</div>
+    <div class=image>image</div>
     <p id=message></p>
     <button onclick="renderLoginPage()">Login</button>
     <button onclick="renderRegisterPage()">Register</button> 
