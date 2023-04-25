d977053e80dca612b79d7832471d8d3778dde193
nelsaj <112635402+nelsaj@users.noreply.github.com>
2023-04-25 14:58:21 +0200

    added requirements to register api

--- loginregister-api/register.php
+++ loginregister-api/register.php
@@ -30,6 +30,38 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){ // make sure its the right method
         send_JSON(["message"=>"Please enter a valid email"], 401);
     }
 
+    //////////////
+
+    // different requirments
+    function tooShort ($input, $value){ // cant be too short
+        if(strlen($input) < 3){
+            send_JSON(["message"=>"The $value needs to be 3 characters or more"], 400); 
+        }
+    }
+    tooShort($post["username"], "username");
+    tooShort($post["password"], "password");
+
+    $splitUsername = str_split($post["username"]);
+    $splitPassword = str_split($post["password"]);
+    $splitEmail = str_split($post["email"]);
+    function incorrectChar ($array, $value){ // characters outside the english alphabet is not allowed
+        $allowed = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+        if($value == "email"){ // email is allowed to have @ and .
+            $allowed[] = '@';
+            $allowed[] = '.';}
+
+        foreach($array as $char){
+            if(!in_array($char, $allowed)){
+                send_JSON(["message"=>"Character not allowed in $value"], 400); 
+            }
+        }
+    }
+    incorrectChar($splitUsername, "username");
+    incorrectChar($splitPassword, "password");
+    incorrectChar($splitEmail, "email");
+    
+    //////////////
+
     if($users != []){ // dont do this if no users yet
         foreach($users as $user){
             if($user["username"] == $post["username"]){ // check if username already exists
