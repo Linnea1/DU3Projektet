f084def2f924fbf7c61df0dcf67350d20f70b5f1
nelsaj <112635402+nelsaj@users.noreply.github.com>
2023-04-25 12:56:55 +0200

    Update categoriesPage.js

--- javaScript/categoriesPage.js
+++ javaScript/categoriesPage.js
@@ -1,11 +1,11 @@
-function renderCategoriesPage(){
+function renderCategoriesPage() {
     let username;
     if (!window.localStorage.getItem("user")) {
         username = "Guest";
     } else {
         username = localStorage.getItem("user");
     }
-    
+
     main.innerHTML = `
         <div class="header">
             <button onclick="">Menu</button>
@@ -15,8 +15,8 @@ function renderCategoriesPage(){
         <div class="categories"></div>
     `;
     const divCategories = document.querySelector(".categories");
-    
-    fetch("www.themealdb.com/api/json/v1/1/categories.php")
+
+    fetch("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
         .then(response => response.json())
         .then(data => {
             data.forEach(category => {
