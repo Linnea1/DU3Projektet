1022deb1b8de276d464969e0acf30cc137c11b3f
nelsaj <112635402+nelsaj@users.noreply.github.com>
2023-05-03 13:27:24 +0200

    fetch funktion

--- javaScript/index.js
+++ javaScript/index.js
@@ -12,3 +12,13 @@ if (!window.localStorage.getItem("user")) {
 } else {
     username = localStorage.getItem("user");
 }
+
+async function fetch(URL, method, body) {
+    let response = await fetch(URL, {
+        method: method,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+    });
+
+    return response;
+}
