376fefa4d3a7dc49f149587c1678e4467f963394
ericalundstrom <erica.lundstrom99@hotmail.com>
2023-05-24 13:27:03 +0200

    Small changes

--- javaScript/menu.js
+++ javaScript/menu.js
@@ -9,25 +9,25 @@ function ShowMenu() {
     let PopUpWindow = document.querySelector("#popUpWindow");
 
     let Startpage = document.createElement("button");
-    let Profile = document.createElement("button");
     let Nightmode = document.createElement("button");
     let LogOut = document.createElement("button");
 
     Startpage.classList.add("Startpage");
     Startpage.classList.add("goBack");
-    Profile.classList.add("Profile");
     Nightmode.classList.add("Nightmode");
     LogOut.classList.add("LogOut");
 
     PopUpWindow.append(Startpage);
-    PopUpWindow.append(Profile);
     PopUpWindow.append(Nightmode);
     PopUpWindow.append(LogOut);
 
     Startpage.textContent = "Startpage";
-    Profile.textContent = "My Profile";
     Nightmode.textContent = "Nightmode";
-    LogOut.textContent = "Log out";
+    if (user.guest) {
+        LogOut.textContent = "Login or register";
+    } else {
+        LogOut.textContent = "Log out";
+    }
 
     PopupMenu.addEventListener("click", e => {
         hideMenu(e)
@@ -38,12 +38,6 @@ function ShowMenu() {
         hideMenu(e)
         renderCategoriesPage()
     });
-
-    Profile.addEventListener("click", e => {
-        newState();
-        hideMenu(e)
-        RenderUserPage(user);
-    })
 
 
     LogOut.addEventListener("click", e => {
