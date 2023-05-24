e35917332b126a4d47990af4966e3c35807ca170
Linnéa <linnea.a.14@gmail.com>
2023-05-24 12:34:05 +0100

    icon

--- api/ratings.php
+++ api/ratings.php
@@ -26,7 +26,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     send_JSON($ratings);
-}
+}elseif ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $post = json_decode(file_get_contents("php://input"), true);
+    $ids = $post['listOfIds'];
 
+    $data = json_decode(file_get_contents($filename), true);
+
+    $ratings = [];
+    foreach ($ids as $id) {
+        $rating = [];
+        foreach ($data as $element) {
+            if ($element['recipeId'] === $id && isset($element['rating'])) {
+                $rating[] = $element['rating'];
+            }
+        }
+
+        if (!empty($rating)) {
+            $average = array_sum($rating) / count($rating);
+            $ratings[] = $average;
+        }else{
+            $ratings[] = 0;
+        }
+    }
+
+    send_JSON($ratings);
+}
 
 ?>
